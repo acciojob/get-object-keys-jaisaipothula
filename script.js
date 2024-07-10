6615decdf5bc8d89cd67191b409bfb3c583e7556
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-10 21:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let student = {
+	name: 'Jaisai Pothula'
+};
+object.prototype.getkeys = function(){
+	return object.keys(this);
+};
+console.log(student.getkeys());
