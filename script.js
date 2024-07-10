6d27d945c112d1dc5c40bbdaf5856410a016500c
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-10 21:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Define the student object with a property called name
 let student = {
-  name: 'John Doe'
+  name: 'Jaisai Pothula'
 };
-
-// Add a method to the Object prototype to get all keys of any object
 Object.prototype.getKeys = function() {
   return Object.keys(this);
 };
 
-// Example usage:
-console.log(student.getKeys());  // Output: ["name"]
+console.log(student.getKeys()); 
